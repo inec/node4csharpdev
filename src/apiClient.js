101b2ddbedf5f88c97089805041b81fbf6c43e10
inec <luyege+github@gmail.com>
2018-12-30 20:09:07 -0600

    'apiClient.js'

---
+++ src/apiClient.js
@@ -0,0 +1,11 @@
+import {request} from 'request-promise-native';
+
+export class ApiClient{
+    getTasks(){
+        return new Promise((resolve,reject)=>{
+            resolve(data);
+            reject(err);
+
+        })
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,1 @@
-import {HelloWorld} from './helloWorld';
-
-
-
-new HelloWorld().sayHello();
-//function sayHi(){}
-
-
-/*sayHi.prototype.hi=function(){
-    console.log("msg3");
-}*/
-//var msg="test";
-//console.log(msg);
-/*sayHi();*/
-
-//const HelloWorl= require('./helloWorld');
-//require
+"use strict"
