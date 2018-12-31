aaa410d01e2c58c4f8969a153e70fc8fdc672edf
inec <luyege+github@gmail.com>
2018-12-31 17:39:34 -0600

    'babel7'

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 const gulp  =  require("gulp");
 const debug  = require("gulp-debug");
-
+const babel  = require("gulp-babel");
 
 gulp.task("build", (done)=>{
 
     //console.log("buiding ....");
     return gulp.src("src/**/*.js")
-        .pipe(debug());
+        .pipe(debug())
+        .pipe( babel(
+            {
+                "presets":["@babel/preset-env"]
+            }
+        )        )
+        .on("error",(err)=>{
+            console.log(`babel error ${err}`);
+        })
+        .pipe(gulp.dest(".compiled"));
     done();
 });
 
