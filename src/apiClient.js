c8ebd280f657b53cb42f1781b92ec7b3e128126d
inec <luyege+github@gmail.com>
2018-12-30 20:21:22 -0600

    'async_works'

--- src/apiClient.js
+++ src/apiClient.js
@@ -1,10 +1,23 @@
-import {request} from 'request-promise-native';
+import {Request} from 'request-promise-native';
 
 export class ApiClient{
     getTasks(){
         return new Promise((resolve,reject)=>{
-            resolve(data);
-            reject(err);
+            var options = {
+                method: "GET",
+                uri:"https://jsonplaceholder.typicode.com/todos/",
+                json:true
+            };
+
+            new Request(options)
+                .then(response=>{
+                    resolve(response);
+                })
+                .catch(err=>{
+                    reject(err)
+                });
+ 
+    
 
         })
     }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 "use strict"
+import {ApiClient} from './apiClient';
+
+var apiClient=new ApiClient();
+
+apiClient.getTasks()
+.then(data=>{
+    console.log(`I got back ${data.length} rows`)
+})
+.catch(err=> {
+    console.log(`bad: ${err}`);
+})
