c033af1285124e724cec3deece4060444b534d3f
inec <luyege+github@gmail.com>
2018-12-30 19:31:58 -0600

    'tdiff'

---
+++ src/helloWorld.js
@@ -0,0 +1,7 @@
+ export class HelloWorld{
+    hi(){
+        console.log("msg4 -5 diif6 ");
+    }
+}
+
+//module.exports= HelloWorld;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import {HelloWorld} from './helloWorld';
+
+
+
+new HelloWorld().hi();
+//function sayHi(){}
+
+
+/*sayHi.prototype.hi=function(){
+    console.log("msg3");
+}*/
+//var msg="test";
+//console.log(msg);
+/*sayHi();*/
+
+//const HelloWorl= require('./helloWorld');
+//require
